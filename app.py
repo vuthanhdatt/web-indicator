082a2c74c079dc2b1b55fd75c85c73f302381c97
vuthanhdatt <80867452+vuthanhdatt@users.noreply.github.com>
2022-01-19 22:50:52 +0700

    Complete homepage

--- app.py
+++ app.py
@@ -1,14 +1,48 @@
 import dash
 import dash_bootstrap_components as dbc
 import dash_labs as dl
+import dash_html_components as html
 
 app = dash.Dash(
     __name__, plugins=[dl.plugins.pages], external_stylesheets=[dbc.themes.YETI]
 )
 
+LOGO = 'https://raw.githubusercontent.com/vuthanhdatt/web-indicator/main/images/brand-img.png'
 
-navbar = dbc.NavbarSimple(
-    dbc.Nav(
+# navbar = dbc.NavbarSimple(
+    
+#     dbc.Nav(
+#         [
+#             dbc.NavLink('Home', href='/'),
+#             dbc.NavLink('Chart', href='/chart'),
+#             dbc.NavLink('Blog', href='/blog')
+            
+#         ],
+#     ),
+#     brand="Indicator Bot",
+#     dark=True,
+#     brand_href= '/',
+#     color="primary",
+#     className="mb-2",
+# )
+
+navbar = dbc.Navbar(
+    dbc.Container(
+        [
+            html.A(
+                # Use row and col to control vertical alignment of logo / brand
+                dbc.Row(
+                    [
+                        dbc.Col(html.Img(src=LOGO, height="30px")),
+                        dbc.Col(dbc.NavbarBrand("Indicator Bot", className="ms-2")),
+                    ],
+                    align="center",
+                    className="g-0",
+                ),
+                href="/",
+                style={"textDecoration": "none"},
+            ),
+            dbc.Nav(
         [
             dbc.NavLink('Home', href='/'),
             dbc.NavLink('Chart', href='/chart'),
@@ -16,12 +50,12 @@ navbar = dbc.NavbarSimple(
             
         ],
     ),
-    brand="indicator Bot",
-    dark=True,
-    brand_href= '/',
+        ]
+    ),
     color="primary",
-    className="mb-2",
+    dark=True,
 )
+
 
 app.layout = dbc.Container(
     [navbar, dl.plugins.page_container],
--- pages/home.py
+++ pages/home.py
@@ -1,8 +1,8 @@
 import dash
 import dash_html_components as html
-
+import base64
 dash.register_page(__name__, path='/')
-
+encoded_image = base64.b64encode(open('images\home-banner.jpg', 'rb').read())
 def layout():
-    # ...
-    return html.H1(children='hompage')
+    
+    return html.Div([html.H1(['Indicator Bot'],className='home-h1'),html.H4(['Where Technology Meets Finance'],className='home-h3')],className='home')
