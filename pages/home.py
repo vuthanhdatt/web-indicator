2d3aa6705733d50cc66eb5934cbfb5a706dd1fa6
vuthanhdatt <80867452+vuthanhdatt@users.noreply.github.com>
2022-01-23 10:11:34 +0700

    Change banner path

--- pages/home.py
+++ pages/home.py
@@ -2,7 +2,7 @@ import dash
 import dash_html_components as html
 import base64
 dash.register_page(__name__, path='/')
-encoded_image = base64.b64encode(open('images\home-banner.jpg', 'rb').read())
+encoded_image = base64.b64encode(open('images/home-banner.jpg', 'rb').read())
 def layout():
     
     return html.Div([html.H1(['Indicator Bot'],className='home-h1'),html.H4(['Where Technology Meets Finance'],className='home-h3')],className='home')
